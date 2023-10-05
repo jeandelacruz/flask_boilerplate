a08b4b2e7f2c11347dd9f525080597accf070fd3
Jeancarlos De la Cruz Criollo <jdelacruzcr94@gmail.com>
2023-10-04 19:03:49 -0500

    Se agrega configuracion para disminuir el consumo de memoria

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -3,6 +3,7 @@ from os import getenv
 
 class BaseConfig:
     SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL')
+    # Rastreo de modificaciones como eventos, consumo de memoria alta por eso lo desactivamos
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
