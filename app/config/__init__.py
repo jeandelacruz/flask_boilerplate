8c7e3721d7cb47b8cc37dfc718f907f229493c3f
Jeancarlos De la Cruz Criollo <jdelacruzcr94@gmail.com>
2023-10-12 20:55:43 -0500

    Se añade configuraciones para el entorno de produccion

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -20,6 +20,13 @@ class DevelopmentConfig(BaseConfig):
     MAIL_DEBUG = True
 
 
+class ProductionConfig(BaseConfig):
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=5)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=3)
+    MAIL_DEBUG = False
+
+
 environment = {
     'development': DevelopmentConfig,
+    'production': ProductionConfig
 }
